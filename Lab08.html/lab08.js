9fa94f5c6b8bf5fb9d588241b9d2bd8d1d9c85e8
modhuriaeishika24 <mbe7@njit.edu>
2024-11-14 18:30:44 -0500

    :)

---
+++ Lab08.html/lab08.js
@@ -0,0 +1,78 @@
+//Q 1
+var Name = "Modhuria"; 
+var age = "18";
+console.log(age, Name);
+//Q 2
+var number = 24;
+
+
+//Q 3
+let RollNo = "15";{
+if(RollNo > 10);
+RollNo = "The RollNo is greater than 10"
+console.log(RollNo)}
+//Q 4
+let = "isMember";{
+    isMember = "True"; 
+    if(isMember = "True");
+    isMember = "Member discount applied";
+    console.log(isMember);
+}
+//Q 5
+let startValue = 50;{
+    startValue ="50";
+    if(startValue = "positive");
+        startValue = "startValue is be displayed";
+         if(startValue = "negative");
+startValue = "startValue is be displayed";
+console.log(startValue);
+}
+//Q 6
+let a = 100;{
+    let b = 50;
+
+    console.log(a);
+
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+}
+// Q 7
+let firstname = "Modhuria";{
+let lastname = "Eishika";
+let age = "18";
+console.log(firstname, lastname, "you are", age, "years old" );
+}
+// Q 8
+let num = "50";{
+    if( num = "positive");
+    if(num < 100);
+    num = "The number is within range";
+    console.log(num);
+    }
+// Q 9
+let hasCar = "ture";{
+let hasLicense = "true";
+let hasInsurance = "true";
+if(hasCar, hasLicense, hasInsurance = "true");
+hasCar, hasLicense, hasInsurance = "you can drive legally";
+console.log(hasCar, hasLicense, hasInsurance);
+}
+//Q 10
+var Num = 21;{
+if(Num = "odd");
+if(Num = "even");
+Num = "the number is even";
+Num = "the numer is odd";
+console.log(Num);
+}
+// Q 11 
+let score = "85";{
+    if(score > 90);
+}
+
+
---
+++ script.js
@@ -0,0 +1,91 @@
+var age = 20;
+var age;
+age = 100; 
+console.log(age);
+
+let userName = "Harry";
+console.log(userName);
+
+let rollNo = 21; 
+
+rollNo = 24;
+console.log(rollNo)
+
+const valuePi = 3.14;
+
+console.log(valuePi);
+
+var height = 5.3;
+
+var height;
+height = 6.0; 
+console.log(age);
+
+let gpa = 4.2; 
+console.log(gpa);
+
+
+let pet = false;
+console.log(pet);
+
+let house = null; 
+console.log(house);
+
+let bignumber= 31427415696161657164976596147017459769765916441844174n;
+
+console.log(bignumber);
+//This is a single line
+
+var firstname = Modhuria;
+console.log(firstname);
+
+var lastname = Eishika;
+console.log(lastname);
+
+let firstName = "Harry";
+let lastName = "Potter";
+console.log(firstName, lastName);
+
+// or use + " " + this to add both names
+
+let a = 100;
+let b = 20;
+a = a + 10;
+//a **= a a = a
+
+//a += 10;  ==> a = a + 10; 
+console.log(a);
+
+console.log(a == b);
+
+console.log( a === b); 
+
+
+let A = true;
+
+let B = false; 
+
+console.log(A && B);
+console.log(A || B);
+
+let houses;
+ let firstname= "Harry";
+
+if(firstname == "Harry"){
+houses = "Gryffindor";
+console.log(houses); 
+}
+else if(number> 80){
+    houses = "Grade B"
+console.log(houses);
+}
+else if(number> 60){
+    houses = "Grade c"
+console.log(houses);
+}
+else if(number> 70){
+    houses = "Grade d"
+console.log(houses);
+}
+
+number > 90 ? "Grade A": "Fail"
