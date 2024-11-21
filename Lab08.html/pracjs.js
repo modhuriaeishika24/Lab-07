a2208f93fb416785bffccc82ed9304d2901dceed
modhuriaeishika24 <mbe7@njit.edu>
2024-11-21 01:33:15 -0500

    Create pracjs.js

---
+++ Lab08.html/pracjs.js
@@ -0,0 +1,44 @@
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+console.log("Hello");
+
+if(a < b);{
+
+}
+
+for (let i=1; i<=5; i++ ){
+//block of code
+//line1 
+console.log("Hello");
+}
+
+let firstname = "Harry Potter";
+
+console.log(firstname[6]);
+
+let Firstname= "Harry";
+let Lastname = "Potter";
+//console.log(`Hello, ${firstname} ${lastname}`);
+
+// console.log(firstname.toUppercase);
+//console.log(firstname)
+
+console.log(firstname.slice(0,5));
+
+let house = "Gryffindor";
+
+console.log(firstname.replace("potter", "Gryffindor"));
+
+console.log
+
+let grade = ['A', 'B', 'C', 'D']
+let finalgrade = [100, 200]
+console.log(grade[1]);
+grade.push(100);
+console.log(grade);
+grade.pop();
+console.log(finalgrade.concat(grade));
+
